11824e5eeb46f3845792204c4a68672ea259bf0d
mohadkaif122344@gmail.com <mohadkaif122344>
2024-12-01 23:01:42 +0530

    lab program 03

---
+++ question.c
@@ -0,0 +1,23 @@
+Programs List
+
+1.	Write a program to find the largest and smallest among three entered numbers 
+    and also display whether the identified largest/smallest number is even or odd.
+2.	Write a program to compute grade of students using if else adder. 
+    The grades are assigned as followed:
+      a.	Marks	      Grade
+      b.	marks<50	    F
+      c.	50≤marks< 60	C
+      d.	60≤marks<70	    B
+      e.	70≤marks<80	    B+
+      f.	80≤marks<90	    A
+      g.   90≤mars≤ 100	    A+
+
+3.	Write a program to check whether the entered year is leap year or
+    not (a year is leap if it is divisible by 4 and divisible by 100 or 400.)
+4.	Write a program to print day name using switch case.
+5.	Write a program to determine whether the input character
+    is capital or small letter, digits or special symbol.
+6.	Write a program to check whether a date is valid or not.
+7.	Write a program to check whether a number is positive,
+    negative or zero using switch case.
+
